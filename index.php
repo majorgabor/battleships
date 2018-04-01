e1d57be27fe773565980196fdf2357df26b17010
Gábor <majorg.96@gmail.com>
2018-04-01 17:47:17 +0200

    [webSocket] match making and some routing fix

--- index.php
+++ index.php
@@ -1,27 +1,42 @@
 <?php
+session_start();
 
-$request_uri = explode('?', $_SERVER['REQUEST_URI'], 2);
+$request_uri = ltrim($_SERVER['REQUEST_URI'], "/");
+$request_uri = rtrim($request_uri, "/");
 
+$request_uri = explode("/", $request_uri);
 
-switch ($request_uri[0]) {
-    case '/battleships/':
-        require 'view/index/index.php';
-        break;
-    case '/battleships/login':
-        require 'view/login/login.php';
-        break;
-    case '/battleships/signup':
-        require 'view/signup/signup.php';
-        break;
-    case '/battleships/account':
-        require 'view/account/account.php';
-        break;
-    case '/battleships/game':
-        require 'view/game/game.php';
-        break;
-    default:
-        header('HTTP/1.0 404 Not Found');
-        // require 'view/404/404.php';
-        echo $request_uri[0];
-        break;
+if(isset($request_uri[3])) {
+    header('HTTP/1.0 404 Not Found');
+    require 'view/404/404.php';
+    echo $_SERVER["REQUEST_URI"];
+    
+} else if(!isset($request_uri[1]) && $request_uri[0] == "battleships") {
+    require 'view/index/index.php';
+    
+} else if($request_uri[1] == "game" && isset($request_uri[2])) {
+    $_SESSION["enemy"] = $request_uri[2];
+    require 'view/game/game.php';
+
+} else if(!isset($request_uri[2])) {
+    switch ($request_uri[1]) {
+        case 'login':
+            require 'view/login/login.php';
+            break;
+        case 'signup':
+            require 'view/signup/signup.php';
+            break;
+        case 'account':
+            require 'view/account/account.php';
+            break;
+        default:
+            header('HTTP/1.0 404 Not Found');
+            require 'view/404/404.php';
+            echo $_SERVER["REQUEST_URI"];
+            break;
+    }
+} else {
+    header('HTTP/1.0 404 Not Found');
+    require 'view/404/404.php';
+    echo $_SERVER["REQUEST_URI"];
 }
